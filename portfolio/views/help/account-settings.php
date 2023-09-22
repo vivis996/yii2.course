7829d6d1f2540e174fb95e7fdf0839cce0951744
Daniel Viveros <dviveros90@gmail.com>
2023-09-22 14:56:39 -0400

    Creating a Controller

---
+++ portfolio/views/help/account-settings.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = 'Account settings';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="help-index">
+  <h1><?= Html::encode($this->title) ?></h1>
+
+  <p>
+    Maecenas nibh eros, tristique at viverra id, facilisis ut turpis. Proin in ligula vel lacus auctor tincidunt.
+    Vestibulum eu nisl purus. Curabitur pharetra turpis nisi, a faucibus ligula eleifend eu. Fusce quis risus ut massa luctus finibus.
+    Cras elementum pretium est eget vehicula. Etiam imperdiet,
+    libero ac mattis fringilla, nunc nisi efficitur quam, vel finibus tellus est in tortor. Pellentesque consequat aliquet viverra.
+    Aliquam faucibus rhoncus lorem. Maecenas nec quam vitae lacus mattis aliquet.
+    Praesent elementum ligula at orci convallis, ut fermentum neque pellentesque. Ut pellentesque libero id quam ornare scelerisque.
+  </p>
+</div>
---
+++ portfolio/views/help/index.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = 'Help Center';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="help-index">
+  <h1><?= Html::encode($this->title) ?></h1>
+
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat pretium massa, ac ornare urna gravida ac.
+    Morbi facilisis a est sit amet tincidunt. Vivamus cursus tortor non ex porttitor, sit amet rutrum sapien tristique.
+    Integer et blandit turpis, vel laoreet justo. Phasellus in leo placerat, tristique tellus in, finibus turpis.
+    Donec sed justo et dui hendrerit placerat ut et nisl. Duis a tellus aliquet, porta mi et, efficitur sem.
+    Cras rhoncus nibh ut ante lacinia, ac lobortis nunc blandit. Maecenas sapien turpis, scelerisque id laoreet non, dapibus id leo.
+    Curabitur sed leo vel diam malesuada posuere. Mauris et turpis lobortis, fermentum libero vitae, efficitur sem.
+    Etiam scelerisque lorem a gravida venenatis. Nulla facilisi. Donec in lectus orci. Curabitur et ante id ex facilisis consectetur.
+  </p>
+  <p>
+    Mauris ac vestibulum justo. Curabitur tempus nisi sit amet lacus euismod fringilla. Quisque id massa vitae nibh lobortis pretium.
+    Proin consequat posuere velit at volutpat. Aliquam ipsum dui, venenatis ut aliquam a, varius sit amet mi.
+    In id mi fringilla neque imperdiet ultricies. Cras nec eros iaculis, viverra ante vitae, vulputate risus.
+    Nam mollis interdum tellus, et lobortis dolor vulputate id. Duis eget convallis ligula, eget luctus urna.
+    Etiam nibh sapien, varius quis nunc id, tempor laoreet ante.
+    Duis malesuada, lectus vel mollis vestibulum, diam nibh condimentum est, ac volutpat lacus ante sed velit.
+    Aliquam sollicitudin dictum gravida. In semper ultrices mauris eget gravida. Etiam nec cursus ante.
+    Morbi blandit, enim et venenatis ullamcorper, risus magna dapibus ex, aliquet pharetra nisi lorem ullamcorper lacus.
+  </p>
+</div>

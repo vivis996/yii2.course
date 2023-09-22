b14131aea14da8f731bab9ab7fb36212dfc83552
Daniel Viveros <dviveros90@gmail.com>
2023-09-22 13:50:23 -0400

    Hello World!

--- portfolio/views/site/index.php
+++ portfolio/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
+        <h1 class="display-4">Hello World!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 

5b1d9cdf9d31423925465c54cc81dbfc45f0cb79
Daniel Viveros <dviveros90@gmail.com>
2023-09-22 14:11:59 -0400

    Creating an Action

--- portfolio/views/layouts/main.php
+++ portfolio/views/layouts/main.php
@@ -42,6 +42,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact us', 'url' => ['/site/contact-us']],
+            ['label' => 'Privacy Policy', 'url' => ['/site/privacy-policy']],
             Yii::$app->user->isGuest
                 ? ['label' => 'Login', 'url' => ['/site/login']]
                 : '<li class="nav-item">'
